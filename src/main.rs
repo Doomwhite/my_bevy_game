9abcaec3b4d06f81f7b293b487fc74371c0b7186
Adinelson Brühmüller <adinelson.bruhmuller@movtech.com.br>
2022-09-08 10:35:26 -0300

    Adds person

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,42 @@
 use bevy::prelude::*;
 
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
 fn main() {
-    App::new().run();
+    App::new()
+        .add_startup_system(add_people)
+        .add_system(hello_world)
+        .add_system(greet_people)
+        .run();
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in query.iter() {
+        println!("Hello {}!", name.0);
+    }
+}
+
+fn add_people(mut commands: Commands) {
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("Adinelson".to_string()));
+
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("Renzo".to_string()));
+
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("Zayna".to_string()));
+}
+
+fn hello_world() {
+    println!("Hello World!");
 }
